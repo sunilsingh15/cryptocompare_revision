958b91f860ab98d70e65238f2425ebae37dbe4c1
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-03 11:45:53 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/cryptoassessment/CryptoassessmentApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cryptoassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoassessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoassessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/cryptoassessment/CryptoassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cryptoassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
