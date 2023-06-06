31d2d19be59da384ddcca1d035f16e7d0ab2f5c1
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-06 17:53:25 +0800

    initial preparation for linking to redis db

---
+++ src/main/java/sg/edu/nus/iss/cryptoassessment/config/RedisConfig.java
@@ -0,0 +1,8 @@
+package sg.edu.nus.iss.cryptoassessment.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfig {
+    
+}
---
+++ src/main/java/sg/edu/nus/iss/cryptoassessment/repository/NewsRepository.java
@@ -0,0 +1,8 @@
+package sg.edu.nus.iss.cryptoassessment.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class NewsRepository {
+    
+}
